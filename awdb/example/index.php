6a0bc9e1aae95a9da4c03a97595fa8c098bcd614
hang <dinghang@ipplus360.com>
2020-06-10 16:19:58 +0800

    优化示例代码

--- awdb/example/index.php
+++ awdb/example/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../autoload.php';
 
 use aw\Db\Reader;
 
-// $ipAddress = '24.24.24.24';
+// $ipAddress = '166.111.4.100';
 $ipAddress = $_GET["ip"];
 $databaseFile = 'C:\\Users\\admin\\Desktop\\allawdb\\IP_city_2020W12_single_WGS84_ipv6.awdb';
 $reader = new Reader($databaseFile);
