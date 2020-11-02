78520fe967e67b0ad03efee0ac7ee8b8e91d1f19
dyh <dyh@123.com>
2020-11-02 18:12:34 +0800

    替换文件

--- awdb/example/index.php
+++ awdb/example/index.php
@@ -1,13 +1,18 @@
 <?php
-
+header("Content-type: text/html; charset=utf-8");
 require __DIR__ . '/../autoload.php';
 
 use aw\Db\Reader;
 
 // $ipAddress = '166.111.4.100';
 $ipAddress = $_GET["ip"];
-$databaseFile = 'C:\\Users\\admin\\Desktop\\allawdb\\IP_city_2020W12_single_WGS84_ipv6.awdb';
+$databaseFile = 'C:\\Users\\admin\\Desktop\\allawdb\\IP_city_single_WGS84_awdb.awdb';
 $reader = new Reader($databaseFile);
-echo json_encode($reader->get($ipAddress));
+$result = json_encode($reader->get($ipAddress),JSON_UNESCAPED_UNICODE);
+$resArr = (array)json_decode($result);
+echo "continent:".$resArr['continent'];
+echo "<br>";
+echo $result;
 // print_r($reader->get($ipAddress));
 $reader->close();
+?>
